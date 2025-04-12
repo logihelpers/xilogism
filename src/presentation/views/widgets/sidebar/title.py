c6b9a25710e6140720fb1640960381d3cd32d467
treppenwitz03 <owenlegrande@gmail.com>
2025-04-12 22:07:56 +0800

    Prep

--- src/presentation/views/widgets/sidebar/title.py
+++ src/presentation/views/widgets/sidebar/title.py
@@ -3,7 +3,7 @@ from flet import *
 class SideBarTitle(Container):
     refs: dict = dict()
     widget_scale: float = 1.0
-    def __init__(self, title: str, is_home: bool = False):
+    def __init__(self, title: str, is_home: bool = False, on_menu_clicked = None):
         super().__init__()
         self.title = title
         self.is_home = is_home
@@ -11,10 +11,23 @@ class SideBarTitle(Container):
             controls = [
                 Text(self.title, weight=FontWeight.W_700, color="black", size=14 * self.widget_scale, no_wrap=False),
                 Container(
-                    content=Image(
-                        src="/icons_light/settings_more.png",
-                        width=16 * self.widget_scale,
-                        height=16 * self.widget_scale
+                    content=PopupMenuButton(
+                        content = Image(
+                            src="/icons_light/settings_more.png",
+                            width=16 * self.widget_scale,
+                            height=16 * self.widget_scale
+                        ),
+                        items = [
+                            PopupMenuItem(
+                                icon = Icons.HIDE_SOURCE,
+                                text = "Hide Group"
+                            ),
+                            PopupMenuItem(
+                                icon = Icons.RESTART_ALT,
+                                text = "Reload"
+                            )
+                        ],
+                        icon_color="black"
                     ),
                     offset=transform.Offset(-0.5, 0),
                     bgcolor="#00191f51",
