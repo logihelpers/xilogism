c016b4da64cdd8d5923ca7725de5f3c196065730
Lyndberg Dimaano <lyndbergdimaano@gmail.com>
2025-05-04 16:28:58 +0800

    Update suna_dark.py

--- src/data/color_values/suna_dark.py
+++ src/data/color_values/suna_dark.py
@@ -1,6 +1,8 @@
 color_values = {
-    "text_color": "#ffffff",
-    "bg_color": "#333333",
-    "sidebar_color": "#262626",
-    "divider_color": "#999999"
+    "text_color": "#000000",
+    "bg_color": "#ededed",
+    "sidebar_color": "#d9d9d9",
+    "divider_color": "#6d6d6d",
+    "accent_color_1": "#4d191f51",
+    "accent_color_2": "#a68c9751"
 }
