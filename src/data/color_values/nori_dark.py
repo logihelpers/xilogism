45a23397d59d53a6738d88c759cb6a0be1a3d6fd
Lyndberg Dimaano <lyndbergdimaano@gmail.com>
2025-05-04 16:26:10 +0800

    Update nori_dark.py

--- src/data/color_values/nori_dark.py
+++ src/data/color_values/nori_dark.py
@@ -2,5 +2,7 @@ color_values = {
     "text_color": "#ffffff",
     "bg_color": "#333333",
     "sidebar_color": "#262626",
-    "divider_color": "#999999"
+    "divider_color": "#999999",
+    "accent_color_1": "#4d1c512e",
+    "accent_color_2": "#a68ca82e"
 }
