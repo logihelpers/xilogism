7b07bdb876c2dc9879e19c0a318dff9eb1310620
Von Homersly Magbojos <hmgbjsdrive@gmail.com>
2025-05-06 23:31:22 +0800

    Update __init__.py

--- src/lang/__init__.py
+++ src/lang/__init__.py
@@ -1,24 +1,24 @@
 from lang.en import text_values as en_val
-from lang.tl import text_values as tag_val
+from lang.tl import text_values as tl_val
 from lang.ceb import text_values as ceb_val
-from lang.ja import text_values as jp_val
-from lang.es import text_values as span_val
+from lang.ja import text_values as ja_val
+from lang.es import text_values as es_val
+from lang.fr import text_values as fr_val 
+from lang.zh import text_values as zh_val 
 
 def get_text_values(language):
     match language:
         case "English":
             return en_val
         case "Tagalog":
-            return tag_val
+            return tl_val
         case "Cebuano":
             return ceb_val
         case "Japanese":
-            return jp_val
+            return ja_val
         case "Spanish":
-            return span_val
+            return es_val
         case "French":
-            return span_val # TEMPORARY
+            return fr_val
         case "Mandarin":
-            return span_val # TEMPORARY
-        case "Brainrot":
-            return span_val # TEMPORARY
+            return zh_val 
